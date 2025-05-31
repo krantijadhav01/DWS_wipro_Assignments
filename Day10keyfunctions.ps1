6451234d5ca83e8bfe2412821136bc753259ed03
krantijadhav01 <jadhavkranti134@gmail.com>
2025-05-31 17:19:15 +0530

    Add files via upload

---
+++ Day10keyfunctions.ps1
@@ -0,0 +1,16 @@
+﻿Show-InstallationPrompt -Message 'Do you want to proceed with the installation?' -ButtonRightText 'Yes' -ButtonLeftText 'No'
+Show-InstallationWelcome -CloseApps 'iexplore,winword,excel'
+
+Show-InstallationWelcome -CloseApps 'winword,excel' -Silent
+
+Show-InstallationProgress -StatusMessage 'Installation in Progress...'
+
+Show-InstallationProgress -StatusMessage "Installation in Progress...`r`nThe installation may take 20 minutes to complete."
+
+Show-InstallationProgress -StatusMessage 'Installation in Progress...'
+
+Show-InstallationProgress -StatusMessage "Installation in Progress...`r`nThe installation may take 20 minutes to complete."
+
+Show-BalloonTip -BalloonTipText 'Installation Started' -BalloonTipTitle 'Application Name'
+
+Show-BalloonTip -BalloonTipIcon 'Info' -BalloonTipText 'Installation Started' -BalloonTipTitle 'Application Name' -BalloonTipTime 1000
