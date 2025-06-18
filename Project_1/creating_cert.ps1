0e2e5db4aeda698698509ef01ccb7e239e9c29b8
krantijadhav01 <jadhavkranti134@gmail.com>
2025-06-18 19:04:42 +0530

    Add files via upload

---
+++ Project_1/creating_cert.ps1
@@ -0,0 +1,2 @@
+﻿$cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=MSIXTestCert" -CertStoreLocation "Cert:\CurrentUser\My"
+Export-PfxCertificate -Cert $cert -FilePath "C:\Users\DELL\Desktop\MyMsixCert.pfx" -Password (ConvertTo-SecureString -String "Test@123" -AsPlainText -Force)
